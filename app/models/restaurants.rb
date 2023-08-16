179cda4ce59332b48dfeb98f2ce730f53e6090e1
Maria Coelho <mariiacoelho25@hotmail.com>
2023-08-16 12:47:30 +0200

    rails new

--- app/models/restaurants.rb
+++
@@ -1,6 +0,0 @@
-class Restaurant < ApplicationRecord
-  has_many :reviews, dependent: :destroy
-
-  validates :name, :address, :category, presence: true
-  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"] }
-end
